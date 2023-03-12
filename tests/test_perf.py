861054c3acc84e2c50bee14073f6a480cd9258fc
Adam Derylo <a.m.derylo@gmail.com>
2023-03-12 13:56:51 +0100

    Further clean up

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -6,6 +6,9 @@ import json
 import socket
 from typing import Optional
 
+MAX_TEAMS = 8
+TEAM_NAME = "PurpleHaze"
+
 
 def configure_exchange(
     exchange_config_file: pathlib.Path,
@@ -76,6 +79,17 @@ def run_test(config_dir: pathlib.Path):
         return
 
 
+def winner(score_board_path: pathlib.Path) -> str:
+    df = pd.read_csv(str(score_board_path))
+    teams = df["Team"].tail(MAX_TEAMS).unique().tolist()
+    assert len(teams) != 0
+
+    tail = df.tail(len(teams))
+    sorted = tail.sort_values(by="ProfitOrLoss", ascending=False)
+
+    return sorted.iloc[0]["Team"]
+
+
 def test_basic():
     prefix = "tests/configs/basic/"
     hdu_port = 8000
@@ -100,22 +114,19 @@ def test_basic():
 
     df = pd.read_csv("tests/logs/basic/score_board.csv")
     tail = df.tail(len(traders))
-    tail.sort_values(by="ProfitOrLoss", ascending=False, inplace=True)
+    tail = tail.sort_values(by="ProfitOrLoss", ascending=False)
 
     assert tail.iloc[0]["Team"] == "PrupleHaze"
 
 
 def test_alone():
     config_dir = pathlib.Path("tests/configs/single")
+
     data = pathlib.Path("data/market_data2.csv")
     performance_test_setup(config_dir, data)
     run_test(config_dir)
 
-    df = pd.read_csv("tests/logs/single/score_board.csv")
-    tail = df.tail(len(traders))
-    tail.sort_values(by="ProfitOrLoss", ascending=False, inplace=True)
-
-    assert tail.iloc[0]["Team"] == "PrupleHaze"
+    assert winner("tests/logs/single/score_board.csv") == TEAM_NAME
 
 
 def test_lol():

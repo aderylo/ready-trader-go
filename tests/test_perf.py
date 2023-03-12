91ad56c4c7e51700cabfe8531a8ccfac46e17c25
Adam Derylo <a.m.derylo@gmail.com>
2023-03-12 18:29:32 +0100

    Add test against eithgt dummies

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -41,3 +41,22 @@ def test_basic(market_data: pathlib.Path, run_with_hdu, speed):
     run_test(test_env_path, run_with_hdu)
 
     assert winner(score_board_path) == TEAM_NAME
+
+
+@pytest.mark.parametrize(
+    "market_data",
+    [
+        pathlib.Path("data/market_data1.csv"),
+        pathlib.Path("data/market_data2.csv"),
+        pathlib.Path("data/market_data3.csv"),
+        pathlib.Path("data/market_data4.csv"),
+    ],
+)
+def test_eight(market_data: pathlib.Path, run_with_hdu, speed):
+    test_env_path = create_test_env("tests/configs/eight", "eight", market_data.stem)
+    score_board_path = test_env_path / "score_board.csv"
+
+    setup_test_env(test_env_path, market_data, speed)
+    run_test(test_env_path, run_with_hdu)
+
+    assert winner(score_board_path) == TEAM_NAME
